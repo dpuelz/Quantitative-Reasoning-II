6e1292967538f60276514bced50d951ed1d793fa
David Puelz <dpuelz@gmail.com>
2025-09-28 15:20:28 -0500

    Update draft.R

--- code/draft.R
+++ code/draft.R
@@ -5,27 +5,3 @@ library(tidyverse)
 # load in the data
 survey <- read.csv("../data/survey.csv")
 
-## Question 1, consider summary() or dim() functions.
-summary(survey)
-which(is.na(survey$ideology))
-
-
-
-
-# Question 2, consider mean() function or doing some data wrangling using group_by()
-survey %>%
-  group_by(year) %>%
-  summarise(mean(ideology,na.rm=TRUE))
-
-
-# Question 3
-survey %>%
-  group_by(draft) %>%
-  summarise(mean(ideology,na.rm=TRUE))
-
-
-# Question 4
-survey %>%
-  group_by(draft,state,year) %>%
-  summarise(mean(ideology,na.rm=TRUE))
-
